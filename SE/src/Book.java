3c55ee1d6fec18d0d73d9be21129e546969800c6
Aiden Lee <yw010804@gmail.com>
2024-05-23 10:27:50 +0900

    first commit

---
+++ SE/src/Book.java
@@ -0,0 +1,12 @@
+
+public class Book {
+    public String title;
+    public String author;
+
+    // 생성자
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
---
+++ SE/src/JunitTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JunitTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetBookInfo() {
+		
+	}
+
+	@Test
+	void testDeleteBook() {
+		
+	}
+
+	@Test
+	void testRegisterBook() {
+		fail("Not yet implemented");
+	}
+
+}
